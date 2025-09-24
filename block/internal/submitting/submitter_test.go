10bb3ac6fd23420027ceb3aec3a3216f3ca701c1
juejinyuxitu <juejinyuxitu@outlook.com>
2025-09-24 22:32:55 +0800

    refactor: omit unnecessary reassignment
    Signed-off-by: juejinyuxitu <juejinyuxitu@outlook.com>

--- block/internal/submitting/submitter_test.go
+++ block/internal/submitting/submitter_test.go
@@ -57,8 +57,6 @@ func TestSubmitter_IsHeightDAIncluded(t *testing.T) {
 	}
 
 	for name, spec := range specs {
-		name := name
-		spec := spec
 		t.Run(name, func(t *testing.T) {
 			included, err := s.IsHeightDAIncluded(spec.height, spec.header, spec.data)
 			if spec.expErr {
