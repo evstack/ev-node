aea5fcec240451d9f883eb3b472cc3bb257d3803
chatton <github.qpeyb@simplelogin.fr>
2025-09-24 12:02:45 +0100

    chore: add doc.go file to evm/test package

---
+++ execution/evm/test/doc.go
@@ -0,0 +1,7 @@
+// Package test contains integration tests for the EVM execution engine.
+// These tests require Docker and the 'evm' build tag to run.
+//
+// To run these tests:
+//
+//	make test-evm
+package test
