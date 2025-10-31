626e3de77b5d8eedcdd03709d3103f9009a5193d
tac0turtle <marko@baricevic.me>
2025-10-31 12:06:30 +0100

    one file

--- pkg/sync/sync_service.go
+++ pkg/sync/sync_service.go
@@ -59,6 +59,38 @@ func WithNotifier(notifier *syncnotifier.Notifier) ServiceOption {
 	}
 }
 
+type publishFn[H header.Header[H]] func([]H)
+
+type instrumentedStore[H header.Header[H]] struct {
+	header.Store[H]
+	publish publishFn[H]
+}
+
+func newInstrumentedStore[H header.Header[H]](
+	delegate header.Store[H],
+	publish publishFn[H],
+) header.Store[H] {
+	if publish == nil {
+		return delegate
+	}
+	return &instrumentedStore[H]{
+		Store:   delegate,
+		publish: publish,
+	}
+}
+
+func (s *instrumentedStore[H]) Append(ctx context.Context, headers ...H) error {
+	if err := s.Store.Append(ctx, headers...); err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		s.publish(headers)
+	}
+
+	return nil
+}
+
 // TODO: when we add pruning we can remove this
 const ninetyNineYears = 99 * 365 * 24 * time.Hour
 
