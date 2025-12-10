106c7be150a1fd5cc32b166e3548490f31438330
Randy Grok <randygrok@gmail.com>
2025-12-10 21:18:46 +0100

    fix problems with CI

--- pkg/da/jsonrpc/client_test.go
+++ pkg/da/jsonrpc/client_test.go
@@ -1,84 +1,48 @@
-package blob
+package blob_test
 
 import (
 	"context"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/filecoin-project/go-jsonrpc"
+	fcjsonrpc "github.com/filecoin-project/go-jsonrpc"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	libshare "github.com/celestiaorg/go-square/v3/share"
+
+	blob "github.com/evstack/ev-node/pkg/da/jsonrpc"
+	"github.com/evstack/ev-node/pkg/da/jsonrpc/mocks"
 )
-
-type mockBlobModule struct {
-	submitHeight uint64
-	submitErr    error
-
-	blob        *Blob
-	proof       *Proof
-	included    bool
-	commitProof *CommitmentProof
-}
-
-func (m *mockBlobModule) Submit(_ context.Context, _ []*Blob, _ *SubmitOptions) (uint64, error) {
-	return m.submitHeight, m.submitErr
-}
-
-func (m *mockBlobModule) Get(_ context.Context, _ uint64, _ libshare.Namespace, _ Commitment) (*Blob, error) {
-	return m.blob, nil
-}
-
-func (m *mockBlobModule) GetAll(_ context.Context, _ uint64, _ []libshare.Namespace) ([]*Blob, error) {
-	return []*Blob{m.blob}, nil
-}
-
-func (m *mockBlobModule) GetProof(_ context.Context, _ uint64, _ libshare.Namespace, _ Commitment) (*Proof, error) {
-	return m.proof, nil
-}
-
-func (m *mockBlobModule) Included(_ context.Context, _ uint64, _ libshare.Namespace, _ *Proof, _ Commitment) (bool, error) {
-	return m.included, nil
-}
-
-func (m *mockBlobModule) GetCommitmentProof(_ context.Context, _ uint64, _ libshare.Namespace, _ []byte) (*CommitmentProof, error) {
-	return m.commitProof, nil
-}
-
-func (m *mockBlobModule) Subscribe(_ context.Context, _ libshare.Namespace) (<-chan *SubscriptionResponse, error) {
-	ch := make(chan *SubscriptionResponse, 1)
-	ch <- &SubscriptionResponse{Height: 11}
-	close(ch)
-	return ch, nil
-}
 
 func newTestServer(t *testing.T, module any) *httptest.Server {
 	t.Helper()
-	rpc := jsonrpc.NewServer()
+	rpc := fcjsonrpc.NewServer()
 	rpc.Register("blob", module)
 	return httptest.NewServer(rpc)
 }
 
 func TestClient_CallsAreForwarded(t *testing.T) {
 	ns := libshare.MustNewV0Namespace([]byte("namespace"))
-	blb, err := NewBlobV0(ns, []byte("data"))
+	blb, err := blob.NewBlobV0(ns, []byte("data"))
 	require.NoError(t, err)
 
-	module := &mockBlobModule{
-		submitHeight: 7,
-		blob:         blb,
-		proof:        &Proof{},
-		included:     true,
-		commitProof:  &CommitmentProof{},
-	}
+	module := mocks.NewMockBlobModule(t)
+	module.On("Submit", mock.Anything, mock.Anything, mock.Anything).Return(uint64(7), nil)
+	module.On("Get", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(blb, nil)
+	module.On("GetAll", mock.Anything, mock.Anything, mock.Anything).Return([]*blob.Blob{blb}, nil)
+	module.On("GetProof", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&blob.Proof{}, nil)
+	module.On("Included", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(true, nil)
+	module.On("GetCommitmentProof", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&blob.CommitmentProof{}, nil)
+
 	srv := newTestServer(t, module)
 	t.Cleanup(srv.Close)
 
-	client, err := NewClient(context.Background(), srv.URL, "", "")
+	client, err := blob.NewClient(context.Background(), srv.URL, "", "")
 	require.NoError(t, err)
 	t.Cleanup(client.Close)
 
-	height, err := client.Blob.Submit(context.Background(), []*Blob{blb}, nil)
+	height, err := client.Blob.Submit(context.Background(), []*blob.Blob{blb}, nil)
 	require.NoError(t, err)
 	require.Equal(t, uint64(7), height)
 
