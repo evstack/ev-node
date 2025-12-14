d7a47a7fa8737346a0efec12b30f8991fa9b0a21
Randy Grok <randygrok@gmail.com>
2025-12-14 23:54:03 +0100

    refactor: improve HTTP client usage in readiness endpoint tests

--- node/single_sequencer_integration_test.go
+++ node/single_sequencer_integration_test.go
@@ -422,6 +422,8 @@ func waitForBlockN(t *testing.T, n uint64, node *FullNode, blockInterval time.Du
 func TestReadinessEndpointWhenBlockProductionStops(t *testing.T) {
 	require := require.New(t)
 
+	httpClient := &http.Client{Timeout: 1 * time.Second}
+
 	config := getTestConfig(t, 1)
 	config.Node.Aggregator = true
 	config.Node.BlockTime = evconfig.DurationWrapper{Duration: 500 * time.Millisecond}
@@ -439,10 +441,14 @@ func TestReadinessEndpointWhenBlockProductionStops(t *testing.T) {
 
 	waitForBlockN(t, 1, node, config.Node.BlockTime.Duration)
 
-	resp, err := http.Get("http://" + config.RPC.Address + "/health/ready")
-	require.NoError(err)
-	require.Equal(http.StatusOK, resp.StatusCode, "Readiness should be READY while producing blocks")
-	resp.Body.Close()
+	require.Eventually(func() bool {
+		resp, err := httpClient.Get("http://" + config.RPC.Address + "/health/ready")
+		if err != nil {
+			return false
+		}
+		defer resp.Body.Close()
+		return resp.StatusCode == http.StatusOK
+	}, 10*time.Second, 100*time.Millisecond, "Readiness should be READY while producing blocks")
 
 	time.Sleep(time.Duration(config.Node.MaxPendingHeadersAndData+2) * config.Node.BlockTime.Duration)
 
@@ -451,7 +457,7 @@ func TestReadinessEndpointWhenBlockProductionStops(t *testing.T) {
 	require.LessOrEqual(height, config.Node.MaxPendingHeadersAndData)
 
 	require.Eventually(func() bool {
-		resp, err := http.Get("http://" + config.RPC.Address + "/health/ready")
+		resp, err := httpClient.Get("http://" + config.RPC.Address + "/health/ready")
 		if err != nil {
 			return false
 		}
