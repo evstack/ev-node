66fbd35de672ad1a9e63eb3653535dc9dd3138c3
github-action-benchmark <github@users.noreply.github.com>
2026-02-18 13:25:59 +0000

    add EVM Contract Roundtrip (go) benchmark result for d2512dd0e01ccf461e59c94906aafd5397e92bb0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771408349411,
+  "lastUpdate": 1771421159085,
   "repoUrl": "https://github.com/evstack/ev-node",
   "entries": {
     "EVM Contract Roundtrip": [
@@ -42,6 +42,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkEvmContractRoundtrip - allocs/op",
             "value": 11512,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "evstack",
+            "username": "evstack"
+          },
+          "committer": {
+            "name": "evstack",
+            "username": "evstack"
+          },
+          "id": "d2512dd0e01ccf461e59c94906aafd5397e92bb0",
+          "message": "feat: introduce e2e benchmarking ",
+          "timestamp": "2026-02-18T12:45:05Z",
+          "url": "https://github.com/evstack/ev-node/pull/3078/commits/d2512dd0e01ccf461e59c94906aafd5397e92bb0"
+        },
+        "date": 1771421156072,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 878594158,
+            "unit": "ns/op\t 1986828 B/op\t   11521 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 878594158,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 1986828,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 11521,
             "unit": "allocs/op",
             "extra": "2 times\n4 procs"
           }
