0174ba1a102e11ca64a6bed9d567447691bad4e9
github-action-benchmark <github@users.noreply.github.com>
2026-04-07 10:54:47 +0000

    add EVM Contract Roundtrip (go) benchmark result for 920f0c9da38deb23fc70a0a717c2cc3f6beaaa35

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1775559286842,
+  "repoUrl": "https://github.com/evstack/ev-node",
+  "entries": {
+    "EVM Contract Roundtrip": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "920f0c9da38deb23fc70a0a717c2cc3f6beaaa35",
+          "message": "build(deps): Bump extractions/setup-just from 3 to 4 (#3227)\n\nBumps [extractions/setup-just](https://github.com/extractions/setup-just) from 3 to 4.\n- [Release notes](https://github.com/extractions/setup-just/releases)\n- [Commits](https://github.com/extractions/setup-just/compare/v3...v4)\n\n---\nupdated-dependencies:\n- dependency-name: extractions/setup-just\n  dependency-version: '4'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-04-07T12:45:43+02:00",
+          "tree_id": "f79670819d05a09daecaee67220f1aa3df8fa261",
+          "url": "https://github.com/evstack/ev-node/commit/920f0c9da38deb23fc70a0a717c2cc3f6beaaa35"
+        },
+        "date": 1775559283482,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 906978622,
+            "unit": "ns/op\t32750984 B/op\t  185428 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 906978622,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 32750984,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 185428,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
