c26c0909b0f17f6bdf1788ef856c253c88d55737
Julien Robert <julien@rbrt.fr>
2026-04-09 14:59:01 +0200

    fix ldflag

--- block/internal/common/consts.go
+++ block/internal/common/consts.go
@@ -1,6 +1,21 @@
 package common
 
-// DefaultMaxBlobSize is the fallback blob size limit used with the DA layer.
+import "strconv"
+
+// defaultMaxBlobSizeStr holds the string representation of the default blob
+// size limit. Override at link time via:
 //
-//	go build -ldflags "-X github.com/evstack/ev-node/block/internal/common.DefaultMaxBlobSize=10485760"
-var DefaultMaxBlobSize uint64 = 5 * 1024 * 1024 // 5MB
+//	go build -ldflags "-X github.com/evstack/ev-node/block/internal/common.defaultMaxBlobSizeStr=125829120"
+var defaultMaxBlobSizeStr = "5242880" // 5 MB
+
+// DefaultMaxBlobSize is the max blob size limit used for blob submission.
+var DefaultMaxBlobSize uint64 = 5 * 1024 * 1024
+
+func init() {
+	v, err := strconv.ParseUint(defaultMaxBlobSizeStr, 10, 64)
+	if err != nil || v == 0 {
+		DefaultMaxBlobSize = 5 * 1024 * 1024
+		return
+	}
+	DefaultMaxBlobSize = v
+}
