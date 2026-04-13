b5aee933d783f47445f73139574ea375415219d4
github-action-benchmark <github@users.noreply.github.com>
2026-04-13 10:09:11 +0000

    add EVM Contract Roundtrip (go) benchmark result for a9e08e8f1477aed0aafbd2c0735affad8a829a07

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1776074950743,
+  "repoUrl": "https://github.com/evstack/ev-node",
+  "entries": {
+    "EVM Contract Roundtrip": [
+      {
+        "commit": {
+          "author": {
+            "email": "marko@baricevic.me",
+            "name": "Marko",
+            "username": "tac0turtle"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "a9e08e8f1477aed0aafbd2c0735affad8a829a07",
+          "message": "chore: docs restyle for new landing page (#3210)\n\ndocs restyle for new landing page",
+          "timestamp": "2026-04-13T11:51:49+02:00",
+          "tree_id": "ecf1aff3a8794a92e55f778917461ec2c2d82837",
+          "url": "https://github.com/evstack/ev-node/commit/a9e08e8f1477aed0aafbd2c0735affad8a829a07"
+        },
+        "date": 1776074947230,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 916119372,
+            "unit": "ns/op\t32017116 B/op\t  176321 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 916119372,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 32017116,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 176321,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
