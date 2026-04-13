15746c2a025ea096f42e220b6f95d881c06631d8
github-action-benchmark <github@users.noreply.github.com>
2026-04-13 10:53:58 +0000

    add EVM Contract Roundtrip (go) benchmark result for cc540d53d9cab422ac069402f859a182d4f6bfd7

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1776077638041,
+  "repoUrl": "https://github.com/evstack/ev-node",
+  "entries": {
+    "EVM Contract Roundtrip": [
+      {
+        "commit": {
+          "author": {
+            "email": "marko@baricevic.me",
+            "name": "Marko",
+            "username": "tac0turtle"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cc540d53d9cab422ac069402f859a182d4f6bfd7",
+          "message": "chore(docs): changes to url (#3242)\n\nchanges to url",
+          "timestamp": "2026-04-13T10:34:40Z",
+          "tree_id": "44d3b51fd58909a207e7d6061fb34d520099e0f2",
+          "url": "https://github.com/evstack/ev-node/commit/cc540d53d9cab422ac069402f859a182d4f6bfd7"
+        },
+        "date": 1776077633457,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 901718222,
+            "unit": "ns/op\t32213956 B/op\t  180700 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 901718222,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 32213956,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 180700,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
