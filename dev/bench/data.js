c7dc6d02f7e2d630d8f77cede258bf610b6dcb8b
github-action-benchmark <github@users.noreply.github.com>
2026-04-14 08:07:48 +0000

    add EVM Contract Roundtrip (go) benchmark result for 2b1784a6bc869d6eec3cd1de04f0a8f9b78b06e1

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1776154068089,
+  "repoUrl": "https://github.com/evstack/ev-node",
+  "entries": {
+    "EVM Contract Roundtrip": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2b1784a6bc869d6eec3cd1de04f0a8f9b78b06e1",
+          "message": "build(deps): Bump actions/upload-artifact from 7.0.0 to 7.0.1 in the patch-updates group (#3246)\n\nbuild(deps): Bump actions/upload-artifact in the patch-updates group\n\nBumps the patch-updates group with 1 update: [actions/upload-artifact](https://github.com/actions/upload-artifact).\n\n\nUpdates `actions/upload-artifact` from 7.0.0 to 7.0.1\n- [Release notes](https://github.com/actions/upload-artifact/releases)\n- [Commits](https://github.com/actions/upload-artifact/compare/v7...v7.0.1)\n\n---\nupdated-dependencies:\n- dependency-name: actions/upload-artifact\n  dependency-version: 7.0.1\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n  dependency-group: patch-updates\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-04-14T09:15:31+02:00",
+          "tree_id": "2a53071d06a7e752aa2cd523a235eda138954133",
+          "url": "https://github.com/evstack/ev-node/commit/2b1784a6bc869d6eec3cd1de04f0a8f9b78b06e1"
+        },
+        "date": 1776154064322,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 910376030,
+            "unit": "ns/op\t32434800 B/op\t  180985 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 910376030,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 32434800,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 180985,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
