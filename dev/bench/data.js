aa58d2fb9dd1bd216e2f86278fa24982b5ce8fe1
github-action-benchmark <github@users.noreply.github.com>
2026-04-28 12:42:06 +0000

    add EVM Contract Roundtrip (go) benchmark result for e83cf6c7447839067e22e1d444214dd645f7afb2

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1777380126372,
+  "repoUrl": "https://github.com/evstack/ev-node",
+  "entries": {
+    "EVM Contract Roundtrip": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e83cf6c7447839067e22e1d444214dd645f7afb2",
+          "message": "build(deps): Bump postcss from 8.5.8 to 8.5.12 in /docs in the npm_and_yarn group across 1 directory (#3292)\n\nbuild(deps): Bump postcss\n\nBumps the npm_and_yarn group with 1 update in the /docs directory: [postcss](https://github.com/postcss/postcss).\n\n\nUpdates `postcss` from 8.5.8 to 8.5.12\n- [Release notes](https://github.com/postcss/postcss/releases)\n- [Changelog](https://github.com/postcss/postcss/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/postcss/postcss/compare/8.5.8...8.5.12)\n\n---\nupdated-dependencies:\n- dependency-name: postcss\n  dependency-version: 8.5.12\n  dependency-type: indirect\n  dependency-group: npm_and_yarn\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-04-28T14:31:09+02:00",
+          "tree_id": "b21bed04cbae419fb1620e0ef1662158cafe5cff",
+          "url": "https://github.com/evstack/ev-node/commit/e83cf6c7447839067e22e1d444214dd645f7afb2"
+        },
+        "date": 1777380122207,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 904735388,
+            "unit": "ns/op\t31698224 B/op\t  175848 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 904735388,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 31698224,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 175848,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
