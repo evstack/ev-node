21f5070517bd3e6f1a3a790fd155bd7ee637e986
github-action-benchmark <github@users.noreply.github.com>
2026-05-12 08:46:12 +0000

    add EVM Contract Roundtrip (go) benchmark result for 27eeb486a21a44030226d5e31472171dd83209b1

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1778575572523,
+  "repoUrl": "https://github.com/evstack/ev-node",
+  "entries": {
+    "EVM Contract Roundtrip": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "27eeb486a21a44030226d5e31472171dd83209b1",
+          "message": "build(deps): Bump uuid from 11.1.0 to 14.0.0 in /docs in the npm_and_yarn group across 1 directory (#3321)\n\nbuild(deps): Bump uuid\n\nBumps the npm_and_yarn group with 1 update in the /docs directory: [uuid](https://github.com/uuidjs/uuid).\n\n\nUpdates `uuid` from 11.1.0 to 14.0.0\n- [Release notes](https://github.com/uuidjs/uuid/releases)\n- [Changelog](https://github.com/uuidjs/uuid/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/uuidjs/uuid/compare/v11.1.0...v14.0.0)\n\n---\nupdated-dependencies:\n- dependency-name: uuid\n  dependency-version: 14.0.0\n  dependency-type: indirect\n  dependency-group: npm_and_yarn\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-05-12T10:37:43+02:00",
+          "tree_id": "c2deadee909f25e0b4776e61e8c6b7319e79107e",
+          "url": "https://github.com/evstack/ev-node/commit/27eeb486a21a44030226d5e31472171dd83209b1"
+        },
+        "date": 1778575567842,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 913727039,
+            "unit": "ns/op\t32946924 B/op\t  186129 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 913727039,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 32946924,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 186129,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
