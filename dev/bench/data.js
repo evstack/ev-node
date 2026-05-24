28e48e324f2de0b42c0b8e8cb504c5905d93ee1c
github-action-benchmark <github@users.noreply.github.com>
2026-05-24 18:23:12 +0000

    add EVM Contract Roundtrip (go) benchmark result for 5ee6483aa360fab4785bb5fe820a1b5a01e8ecf2

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1779646991897,
+  "repoUrl": "https://github.com/evstack/ev-node",
+  "entries": {
+    "EVM Contract Roundtrip": [
+      {
+        "commit": {
+          "author": {
+            "email": "caltechustc@outlook.com",
+            "name": "caltechustc",
+            "username": "caltechustc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5ee6483aa360fab4785bb5fe820a1b5a01e8ecf2",
+          "message": "chore: remove duplicate package import (#3331)\n\nchore: remove duplicate package import and fix some CI issues\n\nSigned-off-by: caltechustc <caltechustc@outlook.com>",
+          "timestamp": "2026-05-24T18:03:26Z",
+          "tree_id": "2dc6474b4cd0dc53fa8ca1155e12691b9e7cbb40",
+          "url": "https://github.com/evstack/ev-node/commit/5ee6483aa360fab4785bb5fe820a1b5a01e8ecf2"
+        },
+        "date": 1779646986805,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 916781524,
+            "unit": "ns/op\t33067568 B/op\t  186084 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 916781524,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 33067568,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 186084,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
