0bf9b17680672d9666406646fb417f6d76e9a24a
github-action-benchmark <github@users.noreply.github.com>
2026-07-10 14:08:53 +0000

    add EVM Contract Roundtrip (go) benchmark result for 34b278a7936d893a6f298d851918e69ff1d0c371

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783692533513,
+  "repoUrl": "https://github.com/evstack/ev-node",
+  "entries": {
+    "EVM Contract Roundtrip": [
+      {
+        "commit": {
+          "author": {
+            "email": "marko@baricevic.me",
+            "name": "Marko",
+            "username": "tac0turtle"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "34b278a7936d893a6f298d851918e69ff1d0c371",
+          "message": "chore: remove ev-grpc (#3380)",
+          "timestamp": "2026-07-10T16:07:08+02:00",
+          "tree_id": "8eb1217b0cb96398f190dd499e134fd1ccda22f7",
+          "url": "https://github.com/evstack/ev-node/commit/34b278a7936d893a6f298d851918e69ff1d0c371"
+        },
+        "date": 1783692526624,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 919321162,
+            "unit": "ns/op\t31393060 B/op\t  170407 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 919321162,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 31393060,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 170407,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
