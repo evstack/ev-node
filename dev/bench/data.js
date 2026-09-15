ce5ad04d597c5a0b14ad32584f290274cf5f60b2
github-action-benchmark <github@users.noreply.github.com>
2026-09-15 13:44:39 +0000

    add EVM Contract Roundtrip (go) benchmark result for 6b40f9768b8b480329ae375471334a535e3656ae

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1789479879319,
+  "repoUrl": "https://github.com/evstack/ev-node",
+  "entries": {
+    "EVM Contract Roundtrip": [
+      {
+        "commit": {
+          "author": {
+            "email": "ginavalent@outlook.com",
+            "name": "ginavalent",
+            "username": "ginavalent"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b40f9768b8b480329ae375471334a535e3656ae",
+          "message": "chore: minor improvement for docs (#3450)\n\nSigned-off-by: ginavalent <ginavalent@outlook.com>",
+          "timestamp": "2026-09-15T15:41:43+02:00",
+          "tree_id": "80337cdcee0da223f5c6517c7384d7db651ab043",
+          "url": "https://github.com/evstack/ev-node/commit/6b40f9768b8b480329ae375471334a535e3656ae"
+        },
+        "date": 1789479873472,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkEvmContractRoundtrip",
+            "value": 912338854,
+            "unit": "ns/op\t 4313292 B/op\t   36731 allocs/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - ns/op",
+            "value": 912338854,
+            "unit": "ns/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - B/op",
+            "value": 4313292,
+            "unit": "B/op",
+            "extra": "2 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkEvmContractRoundtrip - allocs/op",
+            "value": 36731,
+            "unit": "allocs/op",
+            "extra": "2 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
